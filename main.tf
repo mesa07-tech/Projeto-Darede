6a3f6af1365d7620cf7584f05ee85e32135579e8
mesa07-tech <projeto.mesa07@gmail.com>
2024-11-27 16:44:32 -0300

    Atualização providers

--- main.tf
+++ main.tf
@@ -1,14 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.74.0"
+      source  = "hashicorp/aws"
+      version = "5.78.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.34.0"
     }
   }
 }
 
+#================PROVIDERS================
+
 provider "aws" {
-  region = "us-east-1"  
+  region = "us-east-1"
 
   shared_config_files      = var.shared_config_files
   shared_credentials_files = var.shared_credentials_files
@@ -17,14 +24,26 @@ provider "aws" {
   }
 }
 
+provider "kubernetes" {
+  host                   = module.cluster-eks.cluster_endpoint
+  token                  = module.cluster-eks.cluster_token
+  cluster_ca_certificate = base64decode(module.cluster-eks.cluster_certificate_authority_data)
+
+}
+
+
+#================VPC================
+
 module "vpc" {
-  source = "./modules/VPC"
-  vpc_name = "darede-vpc"
-  vpc_cidr = "172.16.0.0/16"
-  azs = ["us-east-1a", "us-east-1b"]
-  public_subnets = ["172.16.1.0/24", "172.16.2.0/24"]
+  source          = "./modules/VPC"
+  vpc_name        = "darede-vpc"
+  vpc_cidr        = "172.16.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["172.16.1.0/24", "172.16.2.0/24"]
   private_subnets = ["172.16.3.0/24", "172.16.4.0/24"]
 }
+
+#=================EKS====================
 
 module "cluster-eks" {
   source            = "./modules/EKS"
@@ -37,15 +56,22 @@ module "cluster-eks" {
   private_subnets   = module.vpc.private_subnets
 }
 
+
+#=================WAF====================
+
 module "waf" {
-  source = "./modules/WAF"
-  waf_name = "darede-waf"
-  waf_scope = "REGIONAL"
+  source       = "./modules/WAF"
+  waf_name     = "darede-waf"
+  waf_scope    = "REGIONAL"
   resource_arn = module.cluster-eks.cluster_arn
+
+  depends_on = [module.cluster-eks]
 }
 
+#=================RDS====================
+
 module "rds" {
-  source = "./modules/RDS"
+ source = "./modules/RDS"
   vpc_cidr_block = module.vpc.vpc_cidr
   vpc_id = module.vpc.vpc_id
   allocated_storage = 20  
